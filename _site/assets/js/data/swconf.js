b57b62e4aac1dcd5b21d4fc909896dcc40f59f5c
Emad Younis <eyounis@gmail.com>
2025-06-29 17:35:31 -0700

    Updates

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1751239447',resources: [
+    cacheName: 'chirpy-1751241923',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
